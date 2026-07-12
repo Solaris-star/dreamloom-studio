b0a25ef28cb222a7abaf959046c1f31fe133ff7c
Solaris-star <820622658@qq.com>
2026-07-12 10:21:49 +0800

    test: 建立服务覆盖率基线

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,7 +1,7 @@
 import eslintPluginVue from 'eslint-plugin-vue'
 
 export default [
-  { ignores: ['**/node_modules', '**/dist', '**/dist-web', '**/out'] },
+  { ignores: ['**/node_modules', '**/coverage', '**/dist', '**/dist-web', '**/out'] },
   ...eslintPluginVue.configs['flat/recommended'],
   {
     files: ['**/*.{js,jsx,vue}'],
